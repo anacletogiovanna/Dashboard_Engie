573d522c0b1b6ea8f853b108360de84e975860e1
Giovanna Anacleto <giovanna18batalha@gmail.com>
2019-07-06 17:40:36 -0300

    Implement the graphs and the history logs

--- engie_dashboard/Controllers/HomeController.cs
+++ engie_dashboard/Controllers/HomeController.cs
@@ -18,14 +18,30 @@ namespace engie_dashboard.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public ActionResult Index()
+        {
+
+            var solicitacoes = _context.Solicitacao.ToList();
+
+            //var graphJson = JsonConvert.SerializeObject(solicitacoes);
+
+            //var pieChart = GraphStatusSolicitacao(solicitacoes);
+
+            return View(solicitacoes);
+        }
+
+        public JsonResult GraphTypes()
         {
 
             var solicitacoes = _context.Solicitacao.ToList().Where(x => x.HoraSolicitacao >= DateTime.Now.Date.AddDays(-1));
-            var countSolicitado = solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Solicitado));
-            var graphJson = JsonConvert.SerializeObject(solicitacoes);
 
-            return View();
+            var countSolicitado = solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Solicitado));
+
+            //var graphJson = JsonConvert.SerializeObject(solicitacoes);
+
+            var pieChart = GraphStatusSolicitacao(solicitacoes);
+
+            return Json(pieChart);
         }
 
         public IActionResult Privacy()
@@ -38,5 +54,27 @@ namespace engie_dashboard.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private List<Array> GraphStatusSolicitacao(IEnumerable<Solicitacao> solicitacoes)
+        {
+            List<Array> data = new List<Array>();
+
+            Array countConfirmado = new object[] { "Confirmado", solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Confirmado)) };
+            data.Add(countConfirmado);
+            Array countEncaminhado = new object[] { "Encaminhado", solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Encaminhado)) };
+            data.Add(countEncaminhado);
+            Array countExecutado = new object[] { "Executado", solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Executado)) };
+            data.Add(countExecutado);
+            Array countRejeitado = new object[] { "Rejeitado", solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Rejeitado)) };
+            data.Add(countRejeitado);
+            Array countSolicitado = new object[] { "Solicitado", solicitacoes.Count(x => x.StatusSolicitacao.Equals(StatusSolicitacaoEnum.Solicitado)) };
+            data.Add(countSolicitado);
+
+            //var PieChart = JsonConvert.SerializeObject(data);
+            return data;
+        }
+
+
     }
+
 }
--- engie_dashboard/wwwroot/js/site.js
+++ engie_dashboard/wwwroot/js/site.js
@@ -2,31 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 // Write your JavaScript code.
 
-var ctx = document.getElementById('solicitacoesChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
+//var ctx = document.getElementById('solicitacoesChart').getContext('2d');
+//var chart = new Chart(ctx, {
+//    // The type of chart we want to create
+//    type: 'pie',
 
-    // The data for our dataset
-    data: { 
-        datasets: [{
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(205, 19, 162)',
-                'rgb(155, 99, 232)'
-            ],
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }],
-        labels: [
-            'Red', 
-            'Yellow', 
-            'Blue'
-        ]
-    },
+//    // The data for our dataset
+//    data: { 
+//        datasets: [{
+//            backgroundColor: [
+//                'rgb(255, 99, 132)',
+//                'rgb(205, 19, 162)',
+//                'rgb(155, 99, 232)'
+//            ],
+            
+//            data: [0, 10, 5, 2, 20, 30, 45]
+//        }],
+//        labels: [
+//            'Red', 
+//            'Yellow', 
+//            'Blue'
+//        ]
+//    },
 
-    // Configuration options go here
-    options: {}
-});
+//    // Configuration options go here
+//    options: {}
+//});
 
 var ctx = document.getElementById('tipoSolicitacao').getContext('2d');
 var chart = new Chart(ctx, {
@@ -37,7 +38,7 @@ var chart = new Chart(ctx, {
     data: {
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
+            backgroundColor: ['rgb(254, 72, 72)', 'rgb(255, 256, 288)', 'rgb(255, 256, 288)'],
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
@@ -45,4 +46,78 @@ var chart = new Chart(ctx, {
 
     // Configuration options go here
     options: {}
+});
+
+//var myLineChart = new Chart(ctx, {
+//    type: 'line',
+//    data: [{
+//        x: 10,
+//        y: 20
+//    }, {
+//        x: 15,
+//        y: 10
+//        }],
+
+//    options: {
+//        scales: {
+//            yAxes: [{
+//                stacked: true
+//            }]
+//        }
+//    }
+//});
+
+var ctx = document.getElementById('myChart').getContext("2d");
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["06:01:00", "06:02:30", "06:03:00", "06:05:50", "06:06:10", "06:07:00", "06:09:00"],
+        datasets: [{
+            label: "Solicitações",
+            borderColor: "#80b6f4",
+            pointBorderColor: "#80b6f4",
+            pointBackgroundColor: "#80b6f4",
+            pointHoverBackgroundColor: "#80b6f4",
+            pointHoverBorderColor: "#80b6f4",
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 3,
+            fill: false,
+            borderWidth: 4,
+            data: [10, 13, 6, 18, 77, 2, 10]
+        }]
+    },
+    options: {
+        legend: {
+            position: "bottom"
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+
+            }],
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: "transparent"
+                },
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
+            }]
+        }
+    }
 });
